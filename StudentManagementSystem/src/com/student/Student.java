1f3ea38638634d2a1c25d6ee1e9f255dda48883e
VaishnaviKenchi <vkenchi5@gmail.com>
2026-03-11 21:17:11 +0530

    Update Student.java

--- StudentManagementSystem/src/com/student/Student.java
+++ StudentManagementSystem/src/com/student/Student.java
@@ -2,23 +2,19 @@ package com.student;
 
 public class Student {
 	
-	//Variables stores student information
 	int id;
 	String name;
 	int age;
-	
-	//Constructor automatically runs when an object is created
+
 	public Student(int id,String name,int age){
 		this.id=id;
 		this.name=name;
 		this.age=age;
-	}
-	
+	}	
 	public void display() {
 		System.out.println("ID:"+id);
 		System.out.println("Name:"+name);
 		System.out.println("Age:"+age);
 		System.out.println("-----------------");
 	}
-
 }
